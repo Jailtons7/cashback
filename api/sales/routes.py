96ea47f1fed0d73d640b0d9e098bdbf552d9355c
Jailton Silva <jss1@ifal.edu.br>
2022-01-26 09:32:44 -0300

    feat: adding jwt authentication to /purchases endpoint

--- api/sales/routes.py
+++ api/sales/routes.py
@@ -1,7 +1,10 @@
+from flask_jwt_extended import jwt_required
+
 from api import app
 from api.sales import views
 
 
 @app.route('/purchases', methods=["GET", "POST"], strict_slashes=False)
+@jwt_required()
 def purchases():
     return views.purchase_view()
