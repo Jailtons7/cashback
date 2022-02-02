cd413ea653d11c1e0513d6ff6fb6994f5df89f28
Jailton Silva <jss1@ifal.edu.br>
2022-02-02 00:03:09 -0300

    refact: removing punctuation from generate_secret_key

--- utils/security.py
+++ utils/security.py
@@ -3,6 +3,6 @@ import secrets
 
 
 def generate_secret_key(n: int = 24):
-    sec = string.digits + string.ascii_letters + string.ascii_uppercase + string.punctuation
+    sec = string.digits + string.ascii_letters + string.ascii_uppercase
     secret_key = ''.join(secrets.choice(sec) for _ in range(n))
     return secret_key
