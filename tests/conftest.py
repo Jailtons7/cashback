ca826a0edd14e9729c7f896a1e1a893b42a1e599
Jailton Silva <jss1@ifal.edu.br>
2022-02-05 18:07:37 -0300

    refact: using create_app with TestSettings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,22 @@
 import pytest
-from urllib.parse import quote_plus
 
 from environs import Env
 from flask_mongoengine import MongoEngine
 
-from api import app, db
+from api import create_app
+from extensions.db import init_db
 from api.authentication.models import User
-from config.settings import Settings
+from config.settings import TestSettings
 
+
+db = init_db()
 db.disconnect()
+
 env = Env()
 env.read_env()
 
 
-class TestSettings(Settings):
-    TESTING = True
-    DB = env.str('DB_TEST', 'test')
-    DB_USER = env.str("DB_USER")
-    DB_PASSWORD = env.str("DB_PASSWORD")
-
-    MONGODB_SETTINGS = {
-        'host': 'mongodb+srv://{user}:{pwd}@cluster0.yuhnh.mongodb.net/{db}?retryWrites=true&w=majority'.format(
-            user=quote_plus(DB_USER), pwd=quote_plus(DB_PASSWORD), db=quote_plus(DB)
-        ),
-    }
+app = create_app(from_config=TestSettings)
 
 
 @pytest.fixture(scope="session")
