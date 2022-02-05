89692d2c3f750be789d2647b2bd6e4f243be99e5
Jailton Silva <jss1@ifal.edu.br>
2022-02-05 18:04:09 -0300

    refact: adding user_blueprint

--- api/authentication/routes.py
+++ api/authentication/routes.py
@@ -1,23 +1,18 @@
-from api import app, jwt
+from flask import Blueprint
+
+from extensions.jwt import init_jwt
 from api.authentication import views
-from api.middlewares import user_view
+
+jwt = init_jwt()
+
+user_blueprint = Blueprint('user_blueprint', __name__)
 
 
-@jwt.user_identity_loader
-def user_identity_lookup(user):
-    return user
-
-
-@jwt.user_lookup_loader
-def user_lookup_callback(_jwt_header, jwt_data):
-    return user_view(_jwt_header, jwt_data)
-
-
-@app.route('/authentication/create-token', methods=['POST'], strict_slashes=False)
+@user_blueprint.route('/authentication/create-token', methods=['POST'], strict_slashes=False)
 def login():
     return views.create_token()
 
 
-@app.route('/authentication/users', methods=['GET', 'POST', 'PUT', 'DELETE'], strict_slashes=False)
+@user_blueprint.route('/authentication/users', methods=['GET', 'POST', 'PUT', 'DELETE'], strict_slashes=False)
 def users():
     return views.users_view()
