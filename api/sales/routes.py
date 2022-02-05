e1e96f67374fbca21cf822304f282f7e56cbec97
Jailton Silva <jss1@ifal.edu.br>
2022-02-05 18:56:51 -0300

    refact: creates sales_blueprint

--- api/sales/routes.py
+++ api/sales/routes.py
@@ -1,16 +1,19 @@
+from flask import Blueprint
 from flask_jwt_extended import jwt_required
 
-from api import app
 from api.sales import views
 
 
-@app.route('/purchases', methods=["GET", "POST"], strict_slashes=False)
+sales_blueprint = Blueprint('sales_blueprint', __name__)
+
+
+@sales_blueprint.route('/purchases', methods=["GET", "POST"], strict_slashes=False)
 @jwt_required()
 def purchases():
     return views.purchase_view()
 
 
-@app.route('/cashback/<string:cpf>', methods=['GET'], strict_slashes=False)
+@sales_blueprint.route('/cashback/<string:cpf>', methods=['GET'], strict_slashes=False)
 @jwt_required()
 def cashback(cpf):
     return views.cashback_view(cpf)
